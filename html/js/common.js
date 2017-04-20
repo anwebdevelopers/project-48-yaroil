075a11a587434ab9c6afecb86b7cf31971130ac9
anwebdevelopers <anwebdev78@gmail.com>
2017-04-20 19:59:06 +0300

    20.04.17

--- html/js/common.js
+++ html/js/common.js
@@ -44,4 +44,41 @@ $(function() {
         fullscreen();
     });
 
+    //---------------------------------------------------------------
+    //Подсчет итемов в слайдере отзывов и вывод на страницу
+    //---------------------------------------------------------------
+    $('.production__slider').each(function() {
+
+    });
+    $('.production__slider-item').each(function() {
+        $(this).prepend('<div class="production__slider-numeral"><span class="production__slider-number">' + ($(this).index() + 1) + '</span>\\<span class="production__slider-quantity">' + $(this).closest('.production__slider').find('.production__slider-item').length + '</span></div>');
+    });
+
+
+    //---------------------------------------------------------------
+    //Слайдер
+    //---------------------------------------------------------------
+
+    $('.production__slider').addClass('swiper-container').wrapInner('<div class="swiper-wrapper"></div>').append('<div class="swiper-pagination"></div><div class="swiper-button-next"></div><div class="swiper-button-prev"></div>');
+    $('.production__slider-item').addClass('swiper-slide');
+    var mySwiper = new Swiper ('.swiper-container', {
+        slidesPerView: 1,
+        speed: 600,
+        autoHeight: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    });
+
+    //------------------------------------------------
+    // Плавный скролл
+    //------------------------------------------------
+
+    $('.scroll').click(function(e) {
+        e.preventDefault();
+        var thisSect = $($(this).attr('href')).offset().top;
+        $('html, body').animate({scrollTop: thisSect }, ((Math.abs(thisSect - $(window).scrollTop()) * 0.1) + 600), 'swing');
+    });
+
 });
