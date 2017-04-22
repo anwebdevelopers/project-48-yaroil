5083f9de97f92eded3b83b3716f0c27e91d0e61c
anwebdevelopers <anwebdev78@gmail.com>
2017-04-22 19:29:52 +0300

    22.04.17

--- html/js/common.js
+++ html/js/common.js
@@ -2,9 +2,9 @@ $(function() {
 
     'use strict';
 
-    //------------------------------------------------------
+    //------------------------------------------------------------
     //Chrome Smooth Scroll
-    //------------------------------------------------------
+    //------------------------------------------------------------
     try {
         $.browserSelector();
         if ($('html').hasClass('chrome')) {
@@ -30,7 +30,7 @@ $(function() {
             var windowHeight = $(window).height(),
                 windowWidth = $(window).width(),
                 headerHeight = $header.height();
-            if (windowHeight > $header.height() && windowWidth > 640) {
+            if (windowHeight > $header.height() && windowWidth > 992) {
                 $header.css({
                     'height' : windowHeight + 'px'
                 });
@@ -60,8 +60,7 @@ $(function() {
         items: 1,
         nav: true,
         navText: '',
-        smartSpeed: 600,
-        autoHeight: true
+        smartSpeed: 600
     });
 
     //---------------------------------------------------------------
@@ -72,13 +71,12 @@ $(function() {
         items: 1,
         nav: true,
         navText: '',
-        smartSpeed: 600,
-        autoHeight: true
+        smartSpeed: 600
     });
 
-    //------------------------------------------------
+    //------------------------------------------------------------
     // Плавный скролл
-    //------------------------------------------------
+    //------------------------------------------------------------
 
     $('.scroll').click(function(e) {
         e.preventDefault();
@@ -86,18 +84,18 @@ $(function() {
         $('html, body').animate({scrollTop: thisSect }, ((Math.abs(thisSect - $(window).scrollTop()) * 0.1) + 600), 'swing');
     });
 
-    //------------------------------------
+    //------------------------------------------------------------
     //Выравнивание блоков по высоте
-    //------------------------------------
+    //------------------------------------------------------------
 
     $(".selection__item-title").equalHeight({
         responsive: true
     });
 
 
-    //---------------------------------------------------------------
+    //------------------------------------------------------------
     //Слайдер gallery
-    //---------------------------------------------------------------
+    //------------------------------------------------------------
     $('.gallery__slider').addClass('owl-carousel').owlCarousel({
         nav: true,
         navText: '',
@@ -116,13 +114,72 @@ $(function() {
         }
     });
 
-    //------------------------------------
+    //------------------------------------------------------------
     //Hover fo IOS
-    //------------------------------------
+    //------------------------------------------------------------
     $('.gallery__item').hover(function() {
         $(this).addClass('active');
     }, function() {
         $(this).removeClass('active');
-    })
+    });
+
+    //------------------------------------------------------------
+    //popup
+    //------------------------------------------------------------
+
+    $('.popup-with-move-anim').magnificPopup({
+        type: 'inline',
+        fixedContentPos: false,
+        fixedBgPos: true,
+        overflowY: 'auto',
+        closeBtnInside: true,
+        preloader: false,
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-slide-bottom'
+    });
+
+    //------------------------------------------------------------
+    // Анимация появления элементов
+    //------------------------------------------------------------
+
+    $('.about__item-icon').animated("zoomIn");
+    $('.about__item-text, .advantages__item').animated("fadeInUp");
+    $('.scheme__item').animated("fadeIn");
+
+    //------------------------------------------------------------
+    // Google Map
+    //------------------------------------------------------------
+    var markerPositions = new Array();
+    	markerPositions[1] = new google.maps.LatLng(57.551825, 39.925649);
+
+    function initialize() {
+        var loc, map;
+
+        loc = new google.maps.LatLng(57.551825, 39.925649);
+
+        map = new google.maps.Map(document.getElementById("map"), {
+             zoom: 16,
+             center: loc,
+             mapTypeId: google.maps.MapTypeId.ROADMAP,
+             scrollwheel: false
+        });
+
+        var iconlink = 'img/';
+        for(var i in markerPositions) {
+          var markerPosition = markerPositions[i];
+
+          var marker = new google.maps.Marker({
+              map: map,
+              position: markerPosition,
+              visible: true,
+              icon: iconlink + 'icon-map.png'
+          });
+        }
+
+        map.set('styles', [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ccc"},{"lightness":20},{"saturation":-97}]}]);
+    }
+    initialize();
+    google.maps.event.addDomListener(window, 'load', initialize);
 
 });
